43dcf86f1fd02b3db39f208c9c1af6b676152aa2
Lait96 <qqqqz96@mail.ru>
2021-05-19 22:30:42 +0300

    volumeset

--- Timer.py
+++ Timer.py
@@ -3,23 +3,30 @@ import pygame
 
 InTimer = True
 MusicOneTime = True
-VolumeLevel = 100
+SettingList = []
 
 file = open('Time.txt', 'r')
-TimerTime = int(file.read())
+for line in file:
+    SettingList.append(int(line))
 file.close()
-
 pygame.mixer.init()
 
 
-def play_sound():
-    pygame.mixer.music.set_volume(0.1)
-    pygame.mixer.music.load("tada.mp3")
-    pygame.mixer.music.play()
+def finish():
     TimerCount['foreground'] = '#D5D5D5'
     TimerCount.place(x=35, y=-3)
     TimerCount['text'] = str("ʕ ᵔᴥᵔ ʔ")
+    play_sound()
 
+def play_sound():
+    global SettingList
+    if SettingList[1] == 100:
+        VolumeLevelInDef = 1
+    else:
+        VolumeLevelInDef = float(str("0" + "." + str(SettingList[1])))
+    pygame.mixer.music.set_volume(VolumeLevelInDef)
+    pygame.mixer.music.load("tada.mp3")
+    pygame.mixer.music.play()
 
 def stop():
     global InTimer
@@ -28,17 +35,16 @@ def stop():
     TimerCount.place(x=17, y=-3)
     TimerCount['text'] = str("¯\_(ツ)_/¯")
 
-
 def start():
     global InTimer
     global MusicOneTime
-    global TimerTime
+    global SettingList
     InTimer = True
     MusicOneTime = True
     TimerCount['foreground'] = '#D5D5D5'
     TimerCount['text'] = str("")
     TimerCount.place(x=40, y=-3)
-    Timer(TimerTime)
+    Timer(SettingList[0])
 
 
 def Timer(TimerTime):
@@ -60,7 +66,7 @@ def Timer(TimerTime):
         window.after(1000, Timer, TimerTime)
     if TimerTime == 0 and MusicOneTime:
         MusicOneTime = False
-        window.after(1000, play_sound)
+        window.after(1000, finish)
 
 
 def drag(event):
@@ -76,40 +82,51 @@ def click(event):
 
 
 def setting():
+    global SettingList
     def TimeForTimer():
-        global TimerTime
+        global SettingList
 
         TimeM = str(MinuteEntry.get())
         TimeS = str(SecondEntry.get())
 
         if TimeM.isdigit() or TimeS.isdigit():
-            TimerTime = 0
+            SettingList[0] = 0
             if TimeM.isdigit():
-                TimerTime += int(TimeM) * 60
+                SettingList[0] += int(TimeM) * 60
             if TimeS.isdigit():
-                TimerTime += int(TimeS)
+                SettingList[0] += int(TimeS)
 
-            file = open('Time.txt', 'w')
-            file.write(str(TimerTime))
-            file.close()
-
-
-    def alphaOff():
+    def TransparencyOff():
         window.attributes('-alpha', 1)
 
-    def alphaOn():
+    def TransparencyOn():
         window.attributes('-alpha', 0.75)
 
     def get_val_motion(event):
-        s1 = VolumeLevelScale.get()
-        print("Motion Number", str(s1))
+        global SettingList
+        SettingList[1] = VolumeLevelScale.get()
+
+
+    def on_closing():
+        global SettingList
+        file = open('Time.txt', 'w')
+        file.write(str(SettingList[0]) + '\n' + str(SettingList[1]) + '\n')
+        file.close()
+        settingWindow.destroy()
+
 
     settingWindow = Toplevel(window)
     settingWindow.geometry('200x100')
 
-    VolumeLevelScale = Scale(settingWindow, orient=HORIZONTAL, length=100, from_=0, to=100, tickinterval=50, resolution=1)
+    ForVolumeLevelLabel = Label(settingWindow, font="Colatemta 8")
+    ForVolumeLevelLabel.grid()
+    ForVolumeLevelLabel['text'] = 'Громкость звука'
+
+    VolumeLevelScale = Scale(settingWindow, orient=HORIZONTAL, length=100, from_=0, to=100,
+                             tickinterval=50, resolution=1)
     VolumeLevelScale.bind("<B1-Motion>", get_val_motion)
     VolumeLevelScale.grid()
+    VolumeLevelScale.set(SettingList[1])
 
     MinuteLabel = Label(settingWindow, font="Colatemta 8")
     MinuteLabel.grid()
@@ -128,17 +145,19 @@ def setting():
     TimeButton = Button(settingWindow, text='Обновить время', command=TimeForTimer)
     TimeButton.grid()
 
-    AlphaOnButton = Button(settingWindow, text='Включить прозрачность', command=alphaOn)
-    AlphaOnButton.grid()
+    TransparencyOnButton = Button(settingWindow, text='Включить прозрачность', command=TransparencyOn)
+    TransparencyOnButton.grid()
 
-    AlphaOffButton = Button(settingWindow, text='Убрать прозрачность', command=alphaOff)
-    AlphaOffButton.grid()
+    TransparencyOffButton = Button(settingWindow, text='Убрать прозрачность', command=TransparencyOff)
+    TransparencyOffButton.grid()
+
+    settingWindow.protocol("WM_DELETE_WINDOW", on_closing)
+
 
 window = Tk()
 window.title("Таймер")
 window.geometry('150x87')
 window['bg'] = "gray22"
-
 
 TimerCount = Label(font="Colatemta 19")
 TimerCount['bg'] = "gray22"
